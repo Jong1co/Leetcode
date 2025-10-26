72bda7486b3af570519b3a95bb9bdf72147e2f2b
Jong1co <95389265+Jong1co@users.noreply.github.com>
2025-10-26 19:31:50 +0900

    Time: 267 ms (46.47%), Space: 95 MB (90.81%) - LeetHub

---
+++ 2731-memoize/2731-memoize.ts
@@ -0,0 +1,25 @@
+type Fn = (...params: number[]) => number
+
+function memoize(fn: Fn): Fn {
+    const cacheMap = {};
+    return function(...args) {
+        const key = args.join(',');
+        if(cacheMap[key] !== undefined) return cacheMap[key]
+
+        const value = fn(...args)
+        cacheMap[key] = value;
+        return value;
+    }
+}
+
+
+/** 
+ * let callCount = 0;
+ * const memoizedFn = memoize(function (a, b) {
+ *	 callCount += 1;
+ *   return a + b;
+ * })
+ * memoizedFn(2, 3) // 5
+ * memoizedFn(2, 3) // 5
+ * console.log(callCount) // 1 
+ */
