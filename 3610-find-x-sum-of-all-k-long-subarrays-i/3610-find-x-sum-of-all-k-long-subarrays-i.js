3b10a437df2260efa0d1543a199d867641c131c9
Jong1co <95389265+Jong1co@users.noreply.github.com>
2025-11-04 19:42:42 +0900

    Time: 14 ms (81.08%), Space: 62.7 MB (75.68%) - LeetHub

---
+++ 3610-find-x-sum-of-all-k-long-subarrays-i/3610-find-x-sum-of-all-k-long-subarrays-i.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findXSum = function(nums, k, x) {
+    const count = nums.length - k + 1
+    const result = []
+    for(let i = 0; i < count; i++){
+        const map = nums.slice(i, i + k).reduce((acc, cur) => {
+            acc[cur] = acc[cur] ? acc[cur] + 1 : 1;
+            return acc
+        }, {})
+
+        const sum = Object.entries(map).sort((a, b) => {
+            if(b[1] === a[1]){
+                return b[0] - a[0]
+            } else {
+                return b[1] - a[1]
+            }
+        }).slice(0, x).reduce((acc, cur) => acc + cur[0] * cur[1],0)
+        result.push(sum)
+    }
+    
+    return result
+};
