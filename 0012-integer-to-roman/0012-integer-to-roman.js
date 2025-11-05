68e905bd9097fa8989352e4a3e69c202b8628e82
Jong1co <95389265+Jong1co@users.noreply.github.com>
2025-11-05 20:58:37 +0900

    Time: 12 ms (19.67%), Space: 62.4 MB (38.18%) - LeetHub

--- 0012-integer-to-roman/0012-integer-to-roman.js
+++ 0012-integer-to-roman/0012-integer-to-roman.js
@@ -2,43 +2,47 @@
  * @param {number} num
  * @return {string}
  */
-var intToRoman = function(num) {
-    let unit = 10;
-    const result = []
+var intToRoman = function (num) {
+  let unit = 1000
+  const result = []
 
-    const romanMap2 = {
-        I:1,
-        V:5,
-        X:10,
-        L:50,
-        C:100,
-        D:500,
-        M:1000
-    }
+  const romanMap2 = {
+    I: 1,
+    V: 5,
+    X: 10,
+    L: 50,
+    C: 100,
+    D: 500,
+    M: 1000,
+  }
 
-    const list2 = [['I', 'V', 'X'], ['X', 'L', 'C'], ['C', 'D', 'M'], ['M', 'M', 'M']]
+  const list2 = [
+    ['M', 'M', 'M'],
+    ['C', 'D', 'M'],
+    ['X', 'L', 'C'],
+    ['I', 'V', 'X'],
+  ]
 
-    while(num !== 0){
-        const r = num % unit;
-        result.push(r)
-        num -= r;
-        unit *= 10;
-    }
+  while (unit >= 1) {
+    const q = Math.floor(num / unit) * unit
+    result.push(q)
+    num %= unit
+    unit /= 10
+  }
 
-    return result.map((item, index) => {
-        const keyList = list2[index]
+  return result
+    .map((item, index) => {
+      if(!item) return ''
+      const keyList = list2[index]
 
-        const [a, b, c] = keyList
-        if(romanMap2[c] - romanMap2[a] === item) return `${a}${c}`
-        if(romanMap2[b] - romanMap2[a] === item) return `${a}${b}`
-        
-        const q = Math.floor(item / romanMap2[b])
-        const r = (item % romanMap2[b]) / 10 ** index
+      const [a, b, c] = keyList
+      if (romanMap2[c] - romanMap2[a] === item) return `${a}${c}`
+      if (romanMap2[b] - romanMap2[a] === item) return `${a}${b}`
 
-        return b.repeat(q) + a.repeat(r)
-    }).reverse().join('')
+      const q = Math.floor(item / romanMap2[b])
+      const r = (item % romanMap2[b]) / 10 ** (3 - index)
 
-    // 9, 5, 4, 1로 해서 아니면?
-    // 9 > x > 5일 경우는 5 * 몫 + 나머지 * 1
-
-};
+      return b.repeat(q) + a.repeat(r)
+    })
+    .join('')
+}
