3d5c5f1a081b0a06bde1e25f0c1c8f9a0251c257
Jong1co <95389265+Jong1co@users.noreply.github.com>
2025-11-13 21:12:05 +0900

    Time: 113 ms (7.68%), Space: 88.8 MB (5.27%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.js
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.js
@@ -10,42 +10,12 @@
  * @return {boolean}
  */
 var isPalindrome = function (head) {
-  let front = new ListNode(head.val)
-  let current = head.next
-  if (current === null || (front.val === current.val && current.next === null)) return true
-
-  while (current) {
-    front = new ListNode(current.val, front)
-    if (front?.val === current.next?.val) {
-      let front_head = front
-      let current_head = current.next
-
-      while (front_head?.val === current_head?.val) {
-        if (front_head === null && current_head === null) {
-          return true
-        } else {
-          current_head = current_head.next
-          front_head = front_head.next
-        }
-      }
+    let s = '', p = '';
+    let cur = head;
+    while(cur){
+        s += cur.val;
+        p = cur.val + p;
+        cur = cur.next;
     }
-
-    if (front?.val === current?.val) {
-      let front_head = front
-      let current_head = current
-
-      while (front_head?.val === current_head?.val) {
-        if (front_head === null && current_head === null) {
-          return true
-        } else {
-          current_head = current_head.next
-          front_head = front_head.next
-        }
-      }
-    }
-
-    current = current.next
-  }
-
-  return false
+    return s == p;
 }
