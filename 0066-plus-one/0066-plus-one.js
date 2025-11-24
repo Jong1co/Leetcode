896682ad8d5facf7794db3b441fd4c8459bb4d90
Jong1co <95389265+Jong1co@users.noreply.github.com>
2025-11-24 21:56:46 +0900

    Time: 0 ms (100%), Space: 54.2 MB (45.86%) - LeetHub

--- 0066-plus-one/0066-plus-one.js
+++ 0066-plus-one/0066-plus-one.js
@@ -3,19 +3,13 @@
  * @return {number[]}
  */
 var plusOne = function(digits) {
-    digits[digits.length - 1] += 1;
-    let i = digits.length - 1
-
-    while(digits[i] === 10 && i > 0){
-        digits[i] = 0;
-        i--
-        digits[i] += 1;
+    for(let i = digits.length - 1; i >= 0; i--){
+        if(digits[i] < 9){
+            digits[i] += 1;
+            return digits
+        }
+        digits[i] = 0
     }
 
-    if(digits[0] === 10){
-        digits[0] = 0
-        return [1].concat(digits)
-    }
-
-    return digits
+    return [1].concat(digits)
 };
