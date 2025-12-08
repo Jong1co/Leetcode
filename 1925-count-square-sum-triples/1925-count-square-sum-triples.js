e09a85478d78122a7816bc3297df8e065a6d11de
Jong1co <95389265+Jong1co@users.noreply.github.com>
2025-12-08 21:39:55 +0900

    Time: 2 ms (88.46%), Space: 57.1 MB (13.46%) - LeetHub

--- 1925-count-square-sum-triples/1925-count-square-sum-triples.js
+++ 1925-count-square-sum-triples/1925-count-square-sum-triples.js
@@ -1,22 +1,18 @@
-/**
- * @param {number} n
- * @return {number}
- */
 var countTriples = function(n) {
-    let result = 0;
-
-    for(let c = 1; c <= n; c++){
-        const c2 = c ** 2
-
-        for(let a = 1; a <= c; a++){
-            const a2 = a ** 2;
-            const b = Math.sqrt(c2 - a2)
-
-            if(b && Number.isInteger(b)) {
-                result++
+    let ans = 0;
+    for (let u = 3; u * u < n * 2; u += 2) {
+        for (let v = 1; v < u && u * u + v * v <= n * 2; v += 2) {
+            if (gcd(u, v) === 1) {
+                ans += Math.floor(n * 2 / (u * u + v * v));
             }
         }
     }
-
-    return result
+    return ans * 2;
 };
+
+function gcd(a, b) {
+    while (a !== 0) {
+        [a, b] = [b % a, a];
+    }
+    return b;
+}
